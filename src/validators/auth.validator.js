fede08c7ecf2107dfd68088e411f9ef6c9478fe5
Ishrz <ishrzz66@gmail.com>
2026-07-07 15:05:17 +0530

    created registor validator

---
+++ src/validators/auth.validator.js
@@ -0,0 +1,29 @@
+import { body, validationResult } from "express-validator";
+
+
+const validator = async (req,res,next)=>{
+    
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors : errors.array() })
+    }
+
+    next()
+}
+
+
+export const registerValidator = [
+  body("email").isEmail().withMessage("email is required"),
+  body("contact")
+    .notEmpty().withMessage("contact number is required")
+    .matches(/^\d{10}$/).withMessage("Contact must be a 10-digit number"),
+  body("password")
+    .isLength({max:6 , min:3}).withMessage("Password must be max 6 and min 3 charachter long")
+    .notEmpty().withMessage("Password is required"),
+  body("fullname")
+    .notEmpty().withMessage("full name is required")
+    .isLength({max:10 , min:3}).withMessage("fullname must be max 10 and min 3 charachter long"),
+
+
+    validator
+];
