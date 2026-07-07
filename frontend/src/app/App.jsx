76ec5d5befb4aa2a9ee464add6fed5882e5f888e
Ishrz <ishrzz66@gmail.com>
2026-07-08 00:02:07 +0530

    react-router setup

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
-
+import { RouterProvider } from 'react-router'
+import { routes } from './app.routes.jsx'
 function App() {
   return (
     <>
-      <h1>hello</h1>
+      <RouterProvider router={routes} />
     </>
   )
 }
