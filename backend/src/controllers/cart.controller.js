342f04f5b2d6c57af07c8a9895d387486a31e10a
Ishrz <ishrzz66@gmail.com>
2026-08-12 20:36:47 +0530

    feat(cart): create cart api

--- backend/src/controllers/cart.controller.js
+++ backend/src/controllers/cart.controller.js
@@ -2,7 +2,7 @@ import { stockOfVariant } from "../dao/product.dao"
 import cartModel from "../models/cart.model"
 import productModel from "../models/product.model"
 
-export const addToCart = (req,res) =>{
+export const addToCart =async (req,res) =>{
     const {productId, variantId} = req.param
     const {quantity=1}= req.body
 
@@ -79,5 +79,23 @@ export const addToCart = (req,res) =>{
         success:true
     })
 
+
+}
+
+export const getCart =async (req,res) => {
+
+    const user = req.user
+
+    let cart = await cartModel.findOne({user:user._id}).populate("items.product")
+
+    if(!cart){
+        cart = await cartModel.create({user:user._id})
+    }
+
+    return res.status(200).json({
+        message:"Cart fetched successfully",
+        success:true,
+        cart
+    })
 
 }
--- backend/src/routes/cart.route.js
+++ backend/src/routes/cart.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { authenticateUser } from "../middlewares/auth.middleware";
-import { addToCart } from "../controllers/cart.controller";
+import { addToCart, getCart } from "../controllers/cart.controller";
 import { cartValidator } from "../validators/cart.validator";
 
 const cartRouter = Router()
@@ -8,9 +8,11 @@ const cartRouter = Router()
 
 // add to cart route
 //POST /api/v1/cart/add/:productId/:variantId
-
 cartRouter.post("/add/:productId/:variantId" , authenticateUser , cartValidator , addToCart)
 
+//get cart and create new cart
+//GET /api/v1/cart/
+cartRouter.get("/", authenticateUser, getCart)
 
 
 export default cartRouter
