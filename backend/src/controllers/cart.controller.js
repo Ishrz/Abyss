2cd2f36dcf349ff1aba4ed8d50a0688c0cabbba8
Ishrz <ishrzz66@gmail.com>
2026-08-15 20:28:32 +0530

    refactor(cart): enhance getCart functionality with aggregation for improved product data retrieval

--- backend/src/controllers/cart.controller.js
+++ backend/src/controllers/cart.controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose"
 import cartModel from "../models/cart.model.js"
 import productModel from "../models/product.model.js"
 
@@ -205,7 +206,99 @@ export const getCart =async (req,res) => {
 
     const user = req.user
 
-    let cart = await cartModel.findOne({user:user._id}).populate("items.product")
+    // let cart = await cartModel.findOne({user:user._id}).populate("items.product")
+
+    let cart = (await cartModel.aggregate([
+      {
+        $match: {
+          user: new mongoose.Types.ObjectId(user._id)
+        }
+      },
+      { $unwind: { path: '$items' } },
+      {
+        $lookup: {
+          from: 'products',
+          localField: 'items.product',
+          foreignField: '_id',
+          as: 'items.product'
+        }
+      },
+      { $unwind: { path: '$items.product', preserveNullAndEmptyArrays: true } },
+      {
+        $addFields: {
+          matchedVariant: {
+            $ifNull: [
+              {
+                $arrayElemAt: [
+                  {
+                    $filter: {
+                      input: { $ifNull: ['$items.product.variants', []] },
+                      as: 'v',
+                      cond: { $eq: ['$$v._id', '$items.variant'] }
+                    }
+                  },
+                  0
+                ]
+              },
+              null
+            ]
+          }
+        }
+      },
+      {
+        $match: {
+          $expr: {
+            $and: [
+              { $ne: ['$items.product', null] },
+              {
+                $or: [
+                  { $eq: ['$items.variant', null] },
+                  { $ne: ['$matchedVariant', null] }
+                ]
+              }
+            ]
+          }
+        }
+      },
+      {
+        $addFields: {
+          'items.product.variants': '$matchedVariant',
+          itemPrice: {
+            price: {
+              $multiply: [
+                {
+                  $ifNull: [
+                    '$matchedVariant.price.amount',
+                    '$items.product.price.amount',
+                    '$items.price.amount',
+                    0
+                  ]
+                },
+                '$items.quantity'
+              ]
+            },
+            currency: {
+              $ifNull: [
+                '$matchedVariant.price.currency',
+                '$items.product.price.currency',
+                '$items.price.currency'
+              ]
+            }
+          }
+        }
+      },
+      {
+        $group: {
+          _id: '$_id',
+          items: { $push: '$items' },
+          totalPrice: { $sum: '$itemPrice.price' },
+          currency: {
+            $first: '$itemPrice.currency'
+          }
+        }
+      }
+    ]))[0]
+
 
     if(!cart){
         cart = await cartModel.create({user:user._id})
